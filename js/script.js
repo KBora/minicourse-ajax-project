5e4eaa04e14f6d044859eda4468700fe5ff23a91
Kian Woo <kian.woo@gmail.com>
2016-02-28 00:06:06 +1100

    Retrieve artcles from NY Times API

--- js/script.js
+++ js/script.js
@@ -12,14 +12,34 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
-
-    // YOUR CODE GOES HERE!
     var street = $('#street').val();
     var city = $('#city').val();
-    var locationParameter = street + ', ' + city;
+    var address = street + ', ' + city;
     var sizeParameter = '&size=900x600';
-    var imgHTML = '<img class="bgimg" src="https://maps.googleapis.com/maps/api/streetview?location=' + locationParameter + sizeParameter + '">';
+    var imgHTML = '<img class="bgimg" src="https://maps.googleapis.com/maps/api/streetview?location=' + address + sizeParameter + '">';
     $body.append(imgHTML);
+
+    // get ny times articles relating to address
+
+    var nytURI = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + address + '&sort=newest&api-key=8050a6b5db4ee4114ff99440f71775be:6:74541255';
+
+    $.getJSON( nytURI , function( data ) {
+
+        console.log()
+      
+        var items = [];
+        $.each( data.response.docs, function( key, val ) {
+            var headline = val.headline.main;
+            var articleURL = val.web_url;
+            items.push( '<li><a href="' + articleURL + '">' + headline + '</a></li>' );
+        });
+
+        $( "<ul/>", {
+            "class": "article",
+            html: items.join( "" )
+        }).appendTo( $('#nytimes-articles') );
+    });
+
 
     return false;
 };
